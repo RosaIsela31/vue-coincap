64e8d244274f651eb29c755ca9ed157490e9c3c3
Rosa <rosaislp@gmail.com>
2020-08-14 00:10:33 -0500

    adding a loader

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "@/assets/css/tailwind.css";
+import Chartick from "vue-chartkick";
+import Chart from "chart.js";
+import { VueSpinners } from "@saeris/vue-spinners";
 
 import router from "@/router";
 import { dollarFilter, percetFilter } from "@/filters";
 
+Vue.use(Chartick.use(Chart));
+Vue.use(VueSpinners);
 Vue.filter("dollar", dollarFilter);
 Vue.filter("percent", percetFilter);
 
@@ -12,5 +17,5 @@ Vue.config.productionTip = false;
 
 new Vue({
   router: router,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
--- src/views/CoinDetail.vue
+++ src/views/CoinDetail.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="flex-col">
+    <div class="flex justify-center">
+      <bounce-loader :loading="isLoading" :color="'#68d391'" :size="100" />
+    </div>
+
     <template v-if="asset.id">
       <div class="flex flex-col sm:flex-row justify-around items-center">
         <div class="flex flex-col items-center">
@@ -78,6 +82,7 @@ export default {
     return {
       asset: {},
       history: [],
+      isLoading: false,
     };
   },
 
@@ -100,11 +105,12 @@ export default {
   methods: {
     getCoin() {
       const id = this.$route.params.id;
-      Promise.all([api.getAsset(id), api.getAssetHistory(id)]).then(
-        ([asset, history]) => {
+      this.isLoading = true;
+      Promise.all([api.getAsset(id), api.getAssetHistory(id)])
+        .then(([asset, history]) => {
           (this.asset = asset), (this.history = history);
-        }
-      );
+        })
+        .finally(() => (this.isLoading = false));
     },
   },
 };
