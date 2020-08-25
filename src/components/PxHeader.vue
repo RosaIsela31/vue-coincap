77e19f71efbc0a7d6a5674f5d7118dcc3bc33b3f
Rosa <rosaislp@gmail.com>
2020-08-25 15:47:10 -0500

    change the path to home

--- src/components/PxHeader.vue
+++ src/components/PxHeader.vue
@@ -4,15 +4,11 @@
       <nav class="flex items-center justify-between flex-wrap bg-green-400 p-6">
         <div class="flex items-center flex-shrink-0 text-white mr-6">
           <px-icon class="mr-2" />
-          <router-link to="/">
-            <span class="font-semibold text-xl tracking-tight"
-              >Platzi Exchange</span
-            >
+          <router-link :to="{name: 'home'}">
+            <span class="font-semibold text-xl tracking-tight">Platzi Exchange</span>
           </router-link>
         </div>
-        <div
-          class="hidden sm:block w-full block flex-grow lg:flex lg:items-center lg:w-auto"
-        >
+        <div class="hidden sm:block w-full block flex-grow lg:flex lg:items-center lg:w-auto">
           <div class="text-sm lg:flex-grow"></div>
         </div>
       </nav>
